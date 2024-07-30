d9896e63929757085e5c5785c1ba71291f0deaec
PuppyCoffee7 <b101112097@tmu.edu.tw>
2024-07-31 01:41:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,28 @@ Created on Mon Jul 22 21:10:37 2024
 """
 
 from flask import Flask, render_template, request
-from keras.models import load_model  # TensorFlow is required for Keras to work
-from PIL import Image, ImageOps  # Install pillow instead of PIL
+from keras.models import load_model
+from PIL import Image, ImageOps, UnidentifiedImageError
 import numpy as np
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/',methods=['Get'])
+@app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html', prediction=None, confidence=None)
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
+    #圖像輸入
     imagefile = request.files['imagefile']
-    #image_path = "./images/" + imagefile.filename
-    #imagefile.save(image_path)
 
-    # Disable scientific notation for clarity
+    #禁用科學記號以提高清晰度
     np.set_printoptions(suppress=True)
 
-    # Load the model
+    #加載模型
     model = load_model("keras_model_500epoch_bo_7525_11.h5")
 
-    # Load the labels
+    #加載標籤
     try:
         with open("labels.txt", "r", encoding='utf-8') as file:
             class_names = file.readlines()
@@ -36,39 +35,41 @@ def predict():
         with open("labels.txt", "r", encoding='latin1') as file:
             class_names = file.readlines()
 
-    # Create the array of the right shape to feed into the keras model
+    #創建一個正確形狀的陣列來輸入到keras模型中
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    # Replace this with the path to your image
-    #image = Image.open(image_path).convert("RGB")
-    image = Image.open(imagefile).convert("RGB")
+    #嘗試讀取圖像，出現錯誤則進行例外處理
+    try:
+        image = Image.open(imagefile).convert("RGB")
+    except UnidentifiedImageError:
+        return render_template('index.html', prediction="nofile", confidence=None)
 
-    # resizing the image to be at least 224x224 and then cropping from the center
+    #將圖像調整到至少224x224，然後從中心裁剪
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-    # turn the image into a numpy array
+    #將圖像轉換為numpy陣列
     image_array = np.asarray(image)
 
-    # Normalize the image
+    #正規化圖像
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # Load the image into the array
+    #將圖像加載到陣列中
     data[0] = normalized_image_array
 
-    # Predicts the model
+    #使用模型來分析圖像
     prediction = model.predict(data)
     index = np.argmax(prediction)
     class_name = class_names[index].strip("01 ")
     confidence_score = prediction[0][index]
+    confidence_score_per = format(confidence_score, ".0%")
     if "Healthy" in class_name:
-        zh_tw_class_name="健康"
+        zh_tw_class_name = "健康"
     else:
-        zh_tw_class_name="有帕金森風險"
+        zh_tw_class_name = "有帕金森風險"
 
-    return render_template('index.html', prediction=zh_tw_class_name, confidence=confidence_score)
+    return render_template('index.html', prediction=zh_tw_class_name, confidence=confidence_score_per)
 
 
-
-if __name__=='__main__':
-    app.run(port=3000,debug=True)
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
